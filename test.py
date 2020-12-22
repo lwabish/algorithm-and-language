55f9d6b713845118828a1cc62e55a31736353eaa
lwabish <wubw@pku.edu.cn>
2020-12-22 18:58:35 +0800

    chore: remove test.py

--- test.py
+++
@@ -1,5 +0,0 @@
-import math
-from functools import reduce
-print(reduce(lambda x, y: x*y, [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-                                2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]))
-print(2**31)
