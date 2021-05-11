8b7585f68c8882b727f3c46e1da61adbe3ec573c
lwabish <wubw@pku.edu.cn>
2021-05-11 21:09:31 +0800

    tree

--- go-leetcode/common/tree/tree.go
+++ go-leetcode/common/tree/tree.go
@@ -1,13 +1,60 @@
 package tree
 
+import "fmt"
+
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
 	Right *TreeNode
 }
 
-func NewTreeByLevel(l []int)*TreeNode{
-	
+// NewTreeByLevel 由层次遍历结果生成一颗二叉树
+func NewTreeByLevel(l []int) (t *TreeNode) {
+	var workQuene []*TreeNode
+	for _, i := range l {
+		if len(workQuene) == 0 {
+			workQuene = append(workQuene, &TreeNode{Val: i})
+			continue
+		}
+		t = workQuene[0]
+
+		if workQuene[0].Left == nil {
+			workQuene[0].Left = &TreeNode{Val: i}
+			workQuene = append(workQuene, workQuene[0].Left)
+			continue
+		}
+
+		if workQuene[0].Right == nil {
+			workQuene[0].Right = &TreeNode{Val: i}
+			workQuene = workQuene[1:]
+			continue
+		}
+
+		return t
+	}
+	return nil
+}
+
+// Draw 画出二叉树的图形结构
+func (t *TreeNode) Draw() {
+	t.draw("", true)
+}
+
+func (t *TreeNode)draw(prefix string, isLeft bool){
+	if !t{
+		return
+	}
+
+	if t.Right{
+		if isLeft{
+			prefix=prefix+"│   "
+		}else{
+			prefix=prefix+"    "
+		}
+		t.Right.draw(prefix, false)
+	}
+
+	fmt.Print()
 }
 
 // LevelOrder 层次遍历
