36bd1ce4f42b5b56513d27fe16d7a74e2a44e64f
lwabish <wubw@pku.edu.cn>
2021-05-11 20:13:02 +0800

    add 872

---
+++ go-leetcode/872叶子相似的树/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	tree "github.com/lwabish/algorithm-and-language/go-leetcode/common/tree"
+)
+
+func main() {
+	t := tree.TreeNode{Val: 1}
+	fmt.Printf("%v", t)
+}
