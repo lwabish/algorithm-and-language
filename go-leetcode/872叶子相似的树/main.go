31055f018653b7f73e7a9cde54d5618097de59a0
lwabish <wubw@pku.edu.cn>
2021-05-20 17:03:51 +0800

    872

--- go-leetcode/872叶子相似的树/main.go
+++ go-leetcode/872叶子相似的树/main.go
@@ -3,10 +3,49 @@ package main
 import (
 	"fmt"
 
-	tree "github.com/lwabish/algorithm-and-language/go-leetcode/common/tree"
+	"github.com/lwabish/go-lib/tree"
 )
 
 func main() {
-	t := tree.TreeNode{Val: 1}
-	fmt.Printf("%v", t)
+	var t1 = tree.NewTree("3 5 1 6 7 4 2 n n n n n n 9 11 n n 8 10")
+	var t2 = tree.NewTree("3 5 1 6 2 9 8 n n 7 4")
+	t1.Draw()
+	t2.Draw()
+	fmt.Println(leafSimilar(t1, t2))
+}
+
+func leafSimilar(root1 *tree.TreeNode, root2 *tree.TreeNode) bool {
+	var result1, result2 = &[]int{}, &[]int{}
+	parseTreeLeaves(root1, result1)
+	parseTreeLeaves(root2, result2)
+
+	fmt.Println(result1, result2)
+	if len(*result1) != len(*result2) {
+		return false
+	}
+
+	for i := 0; i < len(*result1); i++ {
+		if (*result1)[i] != (*result2)[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func parseTreeLeaves(node *tree.TreeNode, result *[]int) {
+	// 叶节点
+	if node.Left == nil && node.Right == nil {
+		*result = append(*result, node.Val)
+	}
+
+	// 左子树
+	if node.Left != nil {
+		parseTreeLeaves(node.Left, result)
+	}
+
+	// 右子树
+	if node.Right != nil {
+		parseTreeLeaves(node.Right, result)
+	}
+
 }
