72c505f5a305f55de5ab137bbebdb36319c0c986
lwabish <wubw@pku.edu.cn>
2021-11-02 09:55:55 +0800

    21+

---
+++ py-leetcode/21+.py
@@ -0,0 +1,48 @@
+from typing import List
+from lwabish.structureutils.listnode import *
+tcs = [
+    (new_listnode([1, 2, 4]), new_listnode([1, 3, 4])),
+    (new_listnode([]), new_listnode([1])),
+    (new_listnode([]), new_listnode([])),
+]
+
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        """
+        合并有序链表
+        递归版本非常优雅但是不太好理解
+        https://leetcode-cn.com/problems/merge-two-sorted-lists/comments/51102
+        """
+        if not l1:
+            return l2
+        elif not l2:
+            return l1
+        # 技巧：dummy head
+        # 第一个节点就可以和其他节点共用一样的逻辑
+        head = ListNode()
+        tail = head
+        while l1 or l2:
+            this_node = ListNode()
+            if not l1:
+                this_node.val = l2.val
+                l2 = l2.next
+            elif not l2:
+                this_node.val = l1.val
+                l1 = l1.next
+            elif l1.val > l2.val:
+                this_node.val = l2.val
+                l2 = l2.next
+            else:
+                this_node.val = l1.val
+                l1 = l1.next
+            tail.next = this_node
+            tail = tail.next
+
+        return head.next
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    for tc in tcs:
+        print_listnode(solution.mergeTwoLists(*tc))
