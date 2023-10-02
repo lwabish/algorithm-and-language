8eb42cb2dc043c89364e2ec5f9e4f83bd1308799
lwabish <wubw@pku.edu.cn>
2023-10-03 02:23:55 +0800

    external

---
+++ the-c-program-language/cha01/09-char-array-1.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#define MAXLINE 1000
+
+int max;
+char line[MAXLINE];
+char longest[MAXLINE];
+
+int getline1(void);
+void copy(void);
+
+int getline1(void)
+{
+    int c, i;
+    extern char line[];
+    for (i = 0; i < MAXLINE - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+    {
+        line[i] = c;
+    }
+
+    if (c == '\n')
+    {
+        line[i] = c;
+        ++i;
+    }
+    return i;
+}
+
+void copy(void)
+{
+    int i;
+    extern char line[], longest[];
+    i = 0;
+    while ((longest[i] = line[i]) != '\0')
+    {
+        ++i;
+    }
+}
+
+int main(void)
+{
+    int len;
+    extern char longest[];
+    max = 0;
+    while ((len = getline1()) > 0)
+    {
+        if (len > max)
+        {
+            copy();
+            max = len;
+        }
+    }
+    if (max > 0)
+        printf("longest: %s", longest);
+    return 0;
+}
