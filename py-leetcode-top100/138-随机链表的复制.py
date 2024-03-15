f208a31432b6e14f0c4cf1fbecaa7c06c1202a94
lwabish <wubw@pku.edu.cn>
2024-03-15 17:28:25 +0800

    138

--- py-leetcode-top100/138-随机链表的复制.py
+++ py-leetcode-top100/138-随机链表的复制.py
@@ -1,7 +1,4 @@
 from typing import List, Optional
-tcs = [
-    (),
-]
 
 
 class Node:
@@ -11,9 +8,30 @@ class Node:
         self.random = random
 
 
+tcs = [
+    (Node(1),),
+]
+
+
+# 不难理解，但是很难想到
+# 理解这题的关键：复制出来的新节点的random节点可能还不存在
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        pass
+        cache = {}
+
+        def copy(head: 'Optional[Node]'):
+            if not head:
+                return None
+            # 查缓存，找到对应关系并返回
+            if cache.get(head):
+                return cache.get(head)
+            this = Node(head.val)
+            # 缓存源节点和新节点的对应关系
+            cache[head] = this
+            this.next = copy(head.next)
+            this.random = copy(head.random)
+            return this
+        return copy(head)
 
 
 if __name__ == '__main__':
