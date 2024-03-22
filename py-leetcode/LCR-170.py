1387f29e7c2bc03a57e55e30b2bdb59d9fbfb632
lwabish <wubw@pku.edu.cn>
2024-03-22 16:04:16 +0800

    WIP

---
+++ py-leetcode/LCR-170.py
@@ -0,0 +1,15 @@
+from typing import List
+tcs = [
+    ([9, 7, 5, 4, 6],),
+]
+
+
+class Solution:
+    def reversePairs(self, record: List[int]) -> int:
+        pass
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    for tc in tcs:
+        print(solution.reversePairs(*tc))
